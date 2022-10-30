da3f5034df0524a7bd53b0031959e58870eab32b
Tehniyat Fatima Fareed <fareedfatima80@gmail.com>
2022-10-30 23:55:34 +0500

    logo successfully changed

--- First_ReactApp/first-react-app/src/index.js
+++ First_ReactApp/first-react-app/src/index.js
@@ -15,3 +15,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react';
+// import ReactDom from 'react-dom/client';
+
+// let a = <h1>Hello Tehniyat Fatima</h1> 
+// ReactDom.render(a , document.getElementById("root"));
